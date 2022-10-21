7d3aa93ccd3073e3d7bcc860c7f8dfe467bb7c19
Andrew Ford <andrew@andrewford.co.nz>
2022-10-21 19:59:13 +1300

    Added get by pokemon by ID Added node-fetch package

--- repository/pokemon.js
+++ repository/pokemon.js
@@ -1,11 +1,20 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-const getAllPokemon = async () => {
-  const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0");
+const getAllPokemon = async (limit, offset) => {
+  const res = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  );
+  const json = await res.json();
+  return json;
+};
+
+const getPokemonById = async (id) => {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
   const json = await res.json();
   return json;
 };
 
 module.exports = {
   getAllPokemon,
+  getPokemonById,
 };
