7f50d056c090c927a67d59b4c33045502754bd76
Andrew Ford <andrew@andrewford.co.nz>
2022-10-21 20:30:44 +1300

    Added page and changed route

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/api/", indexRouter);
 
 const listener = app.listen(8080, function () {
   console.log("Listening on port " + listener.address().port);
