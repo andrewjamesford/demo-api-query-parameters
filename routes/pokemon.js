19e62be1f6f1d8a0ecfeaaf417414360b1064c54
Andrew Ford <andrew@andrewford.co.nz>
2022-10-21 18:49:19 +1300

    var to const removed yarn.lock

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", async (req, res, next) => {
+  console.log(req);
+  try {
+    const postsData = await postsRepository.getPosts();
+    res.status(200).json(postsData);
+  } catch (error) {
+    res.status(500).json({ message: "An error occurred" });
+  }
 });
 
 module.exports = router;
